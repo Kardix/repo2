adce36f27c50753ef70c1b751f553b6969052f2d
Kardix <yourheadissquare@gmail.com>
2018-02-04 14:28:11 +0200

    Added tests

--- Assignment.WebApi.Tests/Controllers/OrdersControllerTest.cs
+++ Assignment.WebApi.Tests/Controllers/OrdersControllerTest.cs
@@ -1,14 +1,19 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.ComponentModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Assignment.WebApi.Controllers;
 using System.Web.Http;
+using Assignment.Core.Model.DTO;
+using Moq;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace Assignment.WebApi.Tests.Controllers
 {
-    [TestClass]
+    [TestFixture]
     public class OrdersControllerTest
     {
-        [TestMethod]
-        public void Get()
+        [Test]
+        public void GetAllOrders()
         {
             // Arrange
             OrdersController controller = new OrdersController();
@@ -16,6 +21,44 @@ namespace Assignment.WebApi.Tests.Controllers
             // Act
             IHttpActionResult result = controller.GetList();
 
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void GetAllItems()
+        {
+            // Arrange
+            ProductsController controller = new ProductsController();
+
+            // Act
+            IHttpActionResult result = controller.GetList();
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void GetOrder()
+        {
+            // Arrange
+            OrdersController controller = new OrdersController();
+
+            // Act
+            IHttpActionResult result = controller.GetOrder(1);
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void GetItem()
+        {
+            // Arrange
+            ProductsController controller = new ProductsController();
+
+            // Act
+            IHttpActionResult result = controller.GetProduct(1);
 
             // Assert
             Assert.IsNotNull(result);
